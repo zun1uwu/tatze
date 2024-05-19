830a72a0459678ab405eb9fc0851eacd90dc4fde
zun1 <zun1@cock.li>
2024-05-19 16:04:15 +0200

    Update run script

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
-make clean && make && ./mkiso.sh && qemu-system-x86_64 tatze.iso
+make clean && make && ./mkiso.sh && qemu-system-x86_64 tatze.iso -d int -D qemu.log -no-reboot -m 128
+
+# qemu-system-i386                                 \
+#   -accel tcg,thread=single                       \
+#   -cpu core2duo                                  \
+#   -m 128                                         \
+#   -no-reboot                                     \
+#   -drive format=raw,media=cdrom,file=myos.iso    \
+#   -serial stdio                                  \
+#   -smp 1                                         \
+#   -usb                                           \
+#   -vga std
