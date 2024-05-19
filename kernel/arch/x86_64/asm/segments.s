32d29b0608ce701bc07d90c261617a736ac56b63
zun1 <zun1@cock.li>
2024-05-19 03:00:48 +0200

    Remove unnecessary GDT descriptors

--- kernel/arch/x86_64/asm/segments.s
+++ kernel/arch/x86_64/asm/segments.s
@@ -3,13 +3,13 @@
 global _reload_segments
 
 _reload_segments:
-        push 0x30                       ; causes triple fault
+        push 0x08                       ; causes triple fault
         lea rax, [rel .reload_cs]
         push rax                        ; causes triple fault
         retfq                           ; causes triple fault
 
 .reload_cs:
-        mov ax, 0x38
+        mov ax, 0x10
         mov ds, ax
         mov es, ax
         mov fs, ax
